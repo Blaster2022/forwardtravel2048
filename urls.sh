043cc40917bd2f1bb45ed04efec81a6dc4c8daee
blaster2022 <105985545+Blaster2022@users.noreply.github.com>
2023-02-26 01:52:16 +0100

    Create urls.sh

---
+++ urls.sh
@@ -0,0 +1,4 @@
+# Store a set of urls in the filesystem
+curl localhost:650/file/urls/pachyderm.io -XPOST
+curl localhost:650/file/urls/news.ycombinator.com -XPOST
+curl localhost:650/file/urls/www.reddit.com -XPOST
